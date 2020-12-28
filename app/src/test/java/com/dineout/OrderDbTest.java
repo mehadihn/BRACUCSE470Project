174b46ca9a0f5e192f712b83c2033625952f7409
Mehadi Hassan <mehadihassan116@gmail.com>
2020-12-28 23:01:28 +0600

    Order Unit test

---
+++ app/src/test/java/com/dineout/OrderDbTest.java
@@ -0,0 +1,12 @@
+package com.dineout;
+
+import com.dineout.code.Model.kitchen.OrderDb;
+
+import org.junit.Before;
+
+public class OrderDbTest {
+    OrderDb orderDb;
+
+    @Before
+    public void 
+}
--- app/src/test/java/com/dineout/OrderTest.java
+++ app/src/test/java/com/dineout/OrderTest.java
@@ -1,4 +1,39 @@
 package com.dineout;
 
+import com.dineout.code.Model.kitchen.Order;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class OrderTest {
+    Order order;
+
+    @Before
+    public void setup() throws Exception{
+        order = new Order ("Cooking" , "Burger");
+    }
+
+    @Test
+    public void testGetStatus() {
+        assertEquals("Cooking", order.getStatus());
+    }
+
+    @Test
+    public void testGetItemName() {
+        assertEquals("Burger", order.getItemName());
+    }
+
+    @Test
+    public void testSetStatus(){
+        order.setStatus("Cooked");
+        assertEquals("Cooked", order.getStatus());
+    }
+
+    public void testSetItemName() {
+        order.setItemName("Ice Cream");
+        assertEquals("Ice Cream", order.getItemName());
+    }
+
 }
