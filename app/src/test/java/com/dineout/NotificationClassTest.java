1600377016a918f16f6998b6bdbec0c366df07a4
Mehadi Hassan <mehadihassan116@gmail.com>
2020-12-29 12:05:02 +0600

    Notification Test

--- app/src/test/java/com/dineout/NotificationClassTest.java
+++ app/src/test/java/com/dineout/NotificationClassTest.java
@@ -1,4 +1,50 @@
 package com.dineout;
 
+import com.dineout.code.Model.hall.DB.NotificationClass;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class NotificationClassTest {
+    NotificationClass notificationClass;
+
+    @Before
+    public void setup() throws Exception{
+        notificationClass = new NotificationClass("Chicken" , true , "13:30");
+    }
+
+    @Test
+    public void testGetItemName() {
+        assertEquals("Chicken" , notificationClass.getItemName());
+    }
+
+    @Test
+    public void testIsRead() {
+        assertEquals(true , notificationClass.isRead());
+    }
+
+    @Test
+    public void testGetTime() {
+        assertEquals("13:30" , notificationClass.getTime());
+    }
+
+    @Test
+    public void testSetItemName() {
+        notificationClass.setItemName("Mutton");
+        assertEquals("Mutton" , notificationClass.getItemName());
+    }
+
+    @Test
+    public void testSetRead() {
+        notificationClass.setRead(false);
+        assertEquals(false , notificationClass.isRead());
+    }
+
+    @Test
+    public void testSetTime() {
+        notificationClass.setTime("14:00");
+        assertEquals("14:00" , notificationClass.getTime());
+    }
 }
